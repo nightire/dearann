fb921cf0b8ac6506646c34367dfb31450c89c2b6
nightire <nightire@very-geek.com>
2014-09-17 21:27:27 +0800

    Add HTML markups for seeds
    Signed-off-by: nightire <nightire@very-geek.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@ Category.create!([
 Post.delete_all
 parlor = Category.where(url_name: 'parlor').first
 parlor.posts.create!([
-  { title: '新朋老友齐相聚', content: 'Some contents...' },
-  { title: '我是麦霸半决赛', content: 'Some contents...' },
+  { title: '新朋老友齐相聚', content: '<p>Some contents...</p>' },
+  { title: '我是麦霸半决赛', content: '<q>Some contents...</q>' },
 ])
 
 bedroom = Category.where(url_name: 'bedroom').first
-bedroom.posts.create! title: '夜猫子的天堂', content: 'Some contents...'
+bedroom.posts.create! title: '夜猫子的天堂', content: '<blockquote>Some contents...</blockquote>'
 
 cuisine = Category.where(url_name: 'cuisine').first
-cuisine.posts.create! title: '英式下午茶＋精美点心', content: 'Some contents...' 
+cuisine.posts.create! title: '英式下午茶＋精美点心', content: '<pre>Some contents...</pre>' 
 
 balcony = Category.where(url_name: 'balcony').first
-balcony.posts.create! title: '瑜伽修行课', content: 'Some contents...'
+balcony.posts.create! title: '瑜伽修行课', content: '<strong>Some contents...</strong>'
